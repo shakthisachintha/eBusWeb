543fa85b17294e30640b9aeb92dc60accf7a3b9e
Malsha6 <mmalsha425@gmail.com>
2020-07-03 06:12:55 +1400

    Updated View Conductor

--- src/Component/Conductor/ViewConductors/ViewConductors.js
+++ src/Component/Conductor/ViewConductors/ViewConductors.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import PermanentDrawerLeft from './../../sidebar/sidebar';
+import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import PermanentDrawerLeft from './../../sidebar/sidebar';
+
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
   },
   body: {
     fontSize: 14,
   },
-
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
@@ -40,42 +46,111 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const useStyles = makeStyles({
+
+
+
+
+const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 700,
   },
-});
+
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#a503fc',
+    //alignItems: 'center',
+    justifyContent: 'center',
+    width: '99vw',
+    height: '125vh',
+    spacing: 0,
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    border: '1px solid',
+    lineHeight: 1.5,
+    borderColor: '#0063cc',
+
+
+  },
+  paper: {
+    
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  welcome: {
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+    fontSize: '1.8rem'
+
+  },
+  ebus: {
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: '2.5rem'
+  },
+
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 8,
+    border: 0,
+    color: 'black',
+    height: 48,
+    width: "30%",
+    padding: '20px 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    justifyContent: 'center'
+  },
+  image: {
+    height: '100px',
+    width: '150px',
+    paddingTop: '10%'
+
+  },
+  card: {
+    maxWidth: '180px',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+  },
+  avatar: {
+    backgroundColor: 'black',
+  },
+}));
 
 export default function ViewConductor() {
   const classes = useStyles();
 
   return (
-    <TableContainer component={classes.Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <PermanentDrawerLeft/>
-    </TableContainer>
+    <Grid container className={classes.root}>
+      <PermanentDrawerLeft />
+      <Grid item xs={12} sm={6} >
+
+
+        <Paper style={
+          {
+            padding: '10px 10px',
+            margin: 10,
+            textAlign: 'center',
+          }}>
+
+          <form>
+
+            <Typography component="h2" variant="" className={classes.welcome}>
+              Conductor Registration
+            </Typography>
+
+
+            
+
+
+
+            <div>
+
+            </div>
+            <br></br>
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }

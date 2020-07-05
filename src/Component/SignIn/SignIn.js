89617002b701cb575aac07089486386310289922
ImalshaRathnaweera <surangirathnaweera1996@gmail.com>
2020-07-05 09:54:56 +0530

    new changes

--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React,{Component} from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -10,73 +9,46 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
-import clsx from 'clsx';
 import G3 from  "../images/G3.png";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: '#a503fc',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100vw',
-    height: '100vh',
-    spacing: 0,
-      
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  welcome:{
-    fontFamily:'Roboto',
-    textAlign: 'center',
-    color:'white',
-    fontSize:'2.5rem',
-      
-  },
-  image:{
-    height:'150px',
-    width:'200px',
-    position:'fixed',
-    
-  },
-  signin:{
-    fontFamily:'Roboto',
-  },
-  card:{
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    marginTop: 20,
-    height:'150px',
-    width:'200px',
-    position:'fixed',
-  },
 
-  button:{
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 8,
-    border: 0,
-    color: 'white',
-    height: 48,
-    width: "50%",
-    padding: '10px 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    marginBottom:'10px',
+class SignIn extends Component{
+  state={
+    user:{
+      email : '',
+      password : ''
+    }
   }
-}));
-
-export default function SignIn() {
-  const classes = useStyles();
-  return (
+  render(){
+    return (
     
-    <Grid container className={classes.root}>
+     <Grid container style={{
+          flexGrow: 1,
+          backgroundColor: '#a503fc',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          height: '100vh',
+          spacing: 0,
+
+      }}>
         <Grid item xs={12} sm={6} >
-            <Typography component="h1" variant="" className={classes.welcome}>
+            <Typography component="h1" variant="" style={{
+              textAlign: 'center',
+              color:'white',
+              fontSize:'2.5rem',
+            }}>
                Welcome Back To eBus
             </Typography>
-            <Card className={classes.card}>
+            <Card style={{
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+              marginTop: 20,
+              height:'150px',
+              width:'200px',
+              position:'fixed',
+
+            }}>
                 <CardMedia
                   component="img"
                   alt="eBus image"
@@ -96,7 +68,7 @@ export default function SignIn() {
             <form >
            
                 <Typography variant={'h5'}>
-                <h3 className={classes.signin}> Sign In </h3>
+                <h3> Sign In </h3>
                 </Typography>
                 <TextField
                     variant="outlined"
@@ -128,7 +100,17 @@ export default function SignIn() {
                     label="Remember me"
                 />
                 </Grid>
-                <Button className={clsx(classes.button)} 
+                <Button style={{
+                  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                  borderRadius: 8,
+                  border: 0,
+                  color: 'white',
+                  height: 48,
+                  width: "50%",
+                  padding: '10px 30px',
+                  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                  marginBottom:'10px',
+                }} 
                 type="submit"
                 variant="contained">
                  {'Sign In'}
@@ -152,3 +134,5 @@ export default function SignIn() {
     </Grid>
   );
 }
+}
+export default SignIn;

a0ff9c86e5bea336f5bbc37e9c40b796b9f5bbfc
tharushiSamara <tharushisamara157@gmail.com>
2020-07-09 14:04:17 +0530

    Completed Dashboard

---
+++ src/Component/Dashboard/Drawer.js
@@ -0,0 +1,233 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import G3 from "../images/G3.png";
+import Link from '@material-ui/core/Link';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    backgroundColor:'#aa00ff',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+
+  const handleClick = () => {
+        setOpen(!open);
+    };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" padding="px">
+            eBus | DashBoard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Card className={classes.card}>
+          <CardMedia
+            component="img"
+            alt="eBus image"
+            height="130"
+            image={G3}
+            title="eBus image"
+          />
+        </Card>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          className={classes.menu}
+        >
+          <Link href="/viewBuses" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="Buses" />
+              </ListItem>
+          </Link >
+
+          <Link href="/viewconductor" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="Conductors" />
+              </ListItem>
+          </Link >
+          
+          <ListItem button onClick={handleClick}>
+            <ListItemText primary="Reports" />
+            { !open ? <ExpandMore /> : <ExpandLess />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+              <Link href="/" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="Generate Report" />
+              </ListItem>
+              </Link >
+              </ListItem>
+              
+              <ListItem button className={classes.nested}>
+              <Link href="/" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="View Report" />
+              </ListItem>
+              </Link >
+              </ListItem>
+              </List>
+          </Collapse>
+              <Link href="/profile" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="Profle" />
+              </ListItem>
+              </Link >
+
+              <Link href="/" variant="body2" underline="none">
+              <ListItem button>
+                <ListItemText primary="Log Out" />
+              </ListItem>
+              </Link >
+           
+        </List>
+        <Divider />
+        
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum.
+          
+        </Typography>
+
+      </main>
+    </div>
+  );
+}
